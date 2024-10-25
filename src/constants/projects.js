0a66da0f2eb33aca6c1e87b222d952d792bee8b4
lazytech614 <derupanjan2021@gmail.com>
2024-10-26 03:51:09 +0530

    update projects

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -3,10 +3,72 @@ import project2 from "../../public/project2.png";
 import project3 from "../../public/project3.png";
 import project4 from "../../public/project4.png";
 import project5 from "../../public/project5.png";
+import project6 from "../../public/chat-app.png";
+import project7 from "../../public/note-sphere.png";
+import project8 from "../../public/quiz-app.png";
 
 export const projects = [
   {
-    id: 1,
+    id: 7,
+    name: "Note Sphere",
+    description:
+      "This is a collaborative note sharing app. This app is made using React, Javascript, and Node.js. This project is a part of my ongoing project. In this project I am working with my three friends together.",
+    techs: [
+      "React",
+      "Javascript",
+      "Tailwind CSS",
+      "MongoDB",
+      "Node",
+      "Express",
+    ],
+    image: project7,
+    url: "https://github.com/lazytech614/Syntax-Error-24",
+  },
+  {
+    id: 6,
+    name: "Chat App",
+    description:
+      "This is a real time chat app made using socket.io. I am currently working on this project.",
+    techs: [
+      "React",
+      "Javascript",
+      "Tailwind CSS",
+      "MongoDB",
+      "Node",
+      "Express",
+      "Socket.io",
+    ],
+    image: project6,
+    url: "https://github.com/lazytech614/Real-Time-Chat-App",
+  },
+  {
+    id: 5,
+    name: "E-Commerce Store",
+    description:
+      "This is an online e-commerce store for clothing. This is I is a full stack web app that I have developed.",
+    techs: [
+      "React",
+      "Javascript",
+      "MongoDB",
+      "Node",
+      "Express",
+      "Stripe",
+      "Tailwind CSS",
+    ],
+    image: project4,
+    url: "https://github.com/lazytech614/E-Commerce-Website-Frontend",
+  },
+  {
+    id: 4,
+    name: "Quiz App",
+    description:
+      "This is a simple quiz app made on react, where user can select topic ans number of questions and see result.",
+    techs: ["React", "Javascript", "Tailwind CSS"],
+    image: project8,
+    url: "https://github.com/lazytech614/Quiz-App",
+  },
+  {
+    id: 3,
     name: "Focus On Today",
     description:
       "A simple to do web app, where you can set three goals and complete them.",
@@ -23,36 +85,11 @@ export const projects = [
     url: "https://lazytech614.github.io/two-good-co/",
   },
   {
-    id: 3,
+    id: 1,
     name: "Mars",
     description: "This is a website for a robotics club in my college.",
     techs: ["HTML & CSS", "Javascript", "GSAP", "Bootstrap"],
     image: project3,
     url: "https://lazytech614.github.io/MaRS/",
-  },
-  {
-    id: 4,
-    name: "E-Commerce Store",
-    description:
-      "This is an online e-commerce store for clothing. This is I is a full stack web app that I have made.",
-    techs: [
-      "React",
-      "Javascript",
-      "MongoDB",
-      "Node",
-      "Express",
-      "Stripe",
-      "Tailwind CSS",
-    ],
-    image: project4,
-    url: "https://lazytech614.github.io/MaRS/",
-  },
-  {
-    id: 5,
-    name: "Content Creation",
-    description: "This is a demo landing page for a content creation company.",
-    techs: ["HTML & CSS", "Javascript", "GSAP"],
-    image: project5,
-    url: "https://lazytech614.github.io/content-creation/",
   },
 ];
